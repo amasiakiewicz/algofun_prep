cc62cc92baffbfab89c06b1c2a8ae42d16126ec3
Arkadiusz Masiakiewicz <amasiakiewicz@gmail.com>
2024-11-14 17:26:24 +0100

    Init

---
+++ src/main/java/algo/fun/algofun_week1/AlgofunWeek1Application.java
@@ -0,0 +1,13 @@
+package algo.fun.algofun_week1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgofunWeek1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgofunWeek1Application.class, args);
+    }
+
+}
---
+++ src/test/java/algo/fun/algofun_week1/AlgofunWeek1ApplicationTests.java
@@ -0,0 +1,13 @@
+package algo.fun.algofun_week1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgofunWeek1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
